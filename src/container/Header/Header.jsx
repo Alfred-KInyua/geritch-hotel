e534df94efec7b0873aed7e682e2544fd70c724d
Kinyua <alfkinyua@gmail.com>
2023-05-19 16:32:37 +0300

    Add header file

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app___wrapper section__padding" id="home">
+    <div className="app__wrapper_info ">
+      <SubHeading title="chase the new flavour " />
+      <h1 className="app__header-h1">The key to fine dining</h1>
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
