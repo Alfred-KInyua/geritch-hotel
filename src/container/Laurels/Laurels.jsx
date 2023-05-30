60062906daf3a4b61ef83be2f42f8bb537325d11
Kinyua <alfkinyua@gmail.com>
2023-05-30 18:02:04 +0300

    add award component

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,18 +1,7 @@
 import { SubHeading } from '../../components';
 import { images, data } from '../../constants';
+import AwardCard from './AwardCard';
 import './Laurels.css';
-
-const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
-  <div className="app__laurels_awards-card">
-    <img src={imgUrl} alt="awards" />
-    <div className="app__laurels_awards-card_content">
-      <p className="p__cormorant" style={{ color: '#DCCA87' }}>
-        {title}
-      </p>
-      <p className="p__opensans">{subtitle}</p>
-    </div>
-  </div>
-);
 
 const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
